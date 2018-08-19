06d3dd59180f1d8e620fef865fb9f60bc919d5f9
Kevin <kklein623@gmail.com>
2018-08-19 16:11:50 -0400

    removed main function and moved to tester project. added gitignore so target folder doesn't get pushed

--- src/main/java/BusinessCardParser.java
+++ src/main/java/BusinessCardParser.java
@@ -1,10 +1,4 @@
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Scanner;
-
-import opennlp.tools.namefind.NameFinderME;
-import opennlp.tools.namefind.TokenNameFinderModel;
 import parsers.*;
 
 public class BusinessCardParser {
@@ -15,49 +9,6 @@ public class BusinessCardParser {
 		contactInfo.setPhoneNumber(PhoneNumberParser.getOfficePhoneNumber(document));
 		contactInfo.setName(NameParser.getName(document));
 		return contactInfo;
-	}
-
-	public static void main(String[] args) throws IOException {
-		
-		boolean inputMode = false;
-		String input = "";
-		
-		final String DIALOGUE = "Welcome. Please type a command: \n"
-				+ "Input - Use the command line to input business card text blocks.\n"
-				+ "Exit  - Exit the Business Card OCR.\n";
-		System.out.print(DIALOGUE);
-
-		Scanner scanIn = new Scanner(System.in);
-		String command = "";
-		
-		System.out.println("Enter command");   
-		while (!command.toLowerCase().equals("exit") | inputMode) {
-			command = scanIn.nextLine();
-			
-			if (inputMode) {
-				if (command.equals("")){
-					inputMode = false;
-					System.out.println("Out of Input Mode");
-					ContactInfo temp = BusinessCardParser.getContactInfo(input);
-					System.out.println(temp.printContactInfo());
-					input = "";
-				}
-				else input += command + "\n";
-			}
-			
-			else if (command.toLowerCase().equals("input")){
-				System.out.println("In Input Mode");
-				System.out.println("Please enter your business card strings. To finish, line break on an empty line");
-				inputMode = true;
-			}
-			
-			else {
-				System.out.println("Command doesn't exist");  
-			}
-			
-		}
-		scanIn.close();
-		System.out.println("Exiting");
 	}
 
 }
